476a6d591a9c5e0f0371e71a4ec92010904ffb28
David GOLAY <dg447135@iut21.u-bourgogne.fr>
2023-03-13 12:11:34 +0100

    added widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:naturalis/Widget/Navbar.dart';
 
 import 'landing.dart';
 
@@ -72,6 +73,7 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
+      drawer: ResponsiveNavBarPage(),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
